5c0a6a048733972ca7d54b10723a2b80c7a43cc1
浅爱 <shallowlove21@gmail.com>
2018-03-14 14:53:41 +0800

    commit

--- NavigationBar/ViewController.m
+++ NavigationBar/ViewController.m
@@ -12,18 +12,48 @@
 
 @end
 
-@implementation ViewController
+@implementation ViewController{
+    UILabel                         *_label;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    CGRect screen = [[UIScreen mainScreen] bounds];
+    CGFloat navigationBarHeight = 44;
+    
+    //创建navigationBar
+    UINavigationBar *navigationBar = [[UINavigationBar alloc] initWithFrame:CGRectMake(0, 40, screen.size.width, navigationBarHeight)];
+    
+    UIBarButtonItem *saveButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemSave target:self action:@selector(save)];
+    UIBarButtonItem *addButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(add)];
+    
+    UINavigationItem *navigationItem = [[UINavigationItem alloc] initWithTitle:@"测试"];
+    navigationItem.leftBarButtonItem = saveButtonItem;
+    navigationItem.rightBarButtonItem = addButtonItem;
+    //添加到navigation上
+    navigationBar.items = @[navigationItem];
+    [self.view addSubview:navigationBar];
+    
+    CGFloat labelWidth = 130;
+    CGFloat labelHeigth = 21;
+    CGFloat labelTopView = 198;
+    
+    _label = [[UILabel alloc] initWithFrame:CGRectMake((screen.size.width- labelWidth) / 2, labelTopView, labelWidth, labelHeigth)];
+    _label.text = @"Label";
+    _label.textAlignment = NSTextAlignmentCenter;
+    
+    [self.view addSubview:_label];
+    
+}
+
+- (void)save {
+    _label.text = @"点击了save";
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)add {
+    _label.text = @"点击了add";
 }
-
 
 @end
